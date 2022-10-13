3dfb757e302f96a8a29d3188051330a494b5af49
Marco Borbon <oscar_b_v@hotmail.com>
2022-10-13 12:51:43 -0700

    __

--- math.c
+++
@@ -1,8 +0,0 @@
-/*Funciones Matematicas para nuestro codigo*/
-
-int doubleInt(int a){
-    if(a > MAX_INT){
-        retunr -1
-    }
-    return a*2;
-}
--- math.h
+++
@@ -1,5 +0,0 @@
-/*encabezado de Math.c*/
-
-#define MAX_INT (2^sizeof(int))/2
-
-int doubleInt(int a);
