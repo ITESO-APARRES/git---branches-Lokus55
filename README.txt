2569a099891754587570453fc3f20e6b80d19724
Marco Borbon <oscar_b_v@hotmail.com>
2022-10-13 13:04:10 -0700

    changes to use math

--- README.txt
+++ README.txt
@@ -3,3 +3,4 @@ PROYECTO DEMO PARA TRABAJAR CON BRANCHES
 
 Nombre del alumno: Oscar Alejandro Borbon Villaseñorder
 
+Fecha de hoy: 13/10/2022
--- hello.c
+++ hello.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "math.h"
 
 
 int main(void){
-    printf("hello");
+    printf("hello World!\n");
+    printf("the double of 5 is %i", doubleInt(5));
     return 0;
 }
